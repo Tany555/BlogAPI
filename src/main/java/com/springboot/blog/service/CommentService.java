945e3969d86eeee5c3d87feb236a2732931bed60
Sohel Tanveer <71164743+Tany555@users.noreply.github.com>
2022-05-31 15:12:34 +0530

    Comment feature added

--- src/main/java/com/springboot/blog/service/CommentService.java
+++ src/main/java/com/springboot/blog/service/CommentService.java
@@ -1,8 +1,18 @@
 package com.springboot.blog.service;
 import com.springboot.blog.payload.CommentDto;
 
+import java.util.List;
+
 
 public interface CommentService {
     CommentDto createComment(long postId, CommentDto commentDto);
+
+    List<CommentDto> getAllCommentsByPostId(long post_id);
+
+    CommentDto getCommentById(Long postId, Long commentId);
+
+    CommentDto updateComment(Long postId, Long commentId, CommentDto commentDto);
+
+    void deleteCommentByID(Long postId, Long commentId);
 
 }
